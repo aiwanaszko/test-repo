a2bc99eddd4c810832b724b2dbdd456e5c3a05c0
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2020-07-05 20:15:48 +0200

    Add basic Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import About from './About/About.js';
+import Home from './Home/Home.js';
+import Contact from './Contact/Contact.js'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to={'/'} >Home </Link>
+            <Link to={'/contact'}>Contact</Link>
+            <Link to={'/about'}>About</Link>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
